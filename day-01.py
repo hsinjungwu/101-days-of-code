2e3cb5417dac4b1c09dd10551fae72383930c6cf
hjwu <hsinjungwu@gmail.com>
2021-03-21 21:58:51 +0800

    Add day 01

---
+++ day-01.py
@@ -0,0 +1,24 @@
+# Day 1-1 Exercise
+print("Day 1 - Python Print Function")
+print("The function is declared like this:")
+print("print('what to print')")
+
+# Day 1-2 Exercise
+print("Day 1 - String Manipulation")
+print('String Concatenation is done with the "+" sign.')
+print('e.g. print("Hello " + "world")')
+print("New lines can be created with a backslash and n.")
+
+# Day 1-3 Exercise
+print(len(input("What is your name ? ")))
+
+# Day 1-4 Exercise
+t = a
+a = b
+b = t
+
+# Project : Band Name Generator
+print("Hello~")
+city = input("What the name of city you grow up in?\n")
+pet_name = input("What is your pet's name ?\n")
+print("Your band name could be " + city + " " + pet_name)
---
+++ readme.md
@@ -0,0 +1,5 @@
+# 100 Days of Code
+
+## The Complete Python Pro Bootcamp for 2021
+
+課程網址 : https://www.udemy.com/course/100-days-of-code
